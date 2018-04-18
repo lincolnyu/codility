8cf80e9334f054c2a9f50118c0c3899728c8397a
lincolnyu <g@b1UtLhb9eN>
2018-04-18 22:54:52 +1000

    Created stubs for lesson 14.

---
+++ codility/Lessons/Lesson14/MinMaxDivision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using codility.TestFramework;
+
+namespace codility.Lessons.Lesson14
+{
+    class MinMaxDivision : ITestee
+    {
+        int Solve(int K, int M, int[] A)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object Run(params object[] args)
+            => Solve((int)args[0], (int)args[1], (int[])args[2]);
+
+        public class Tester : BaseSelfTester<MinMaxDivision>
+        {
+            public override IEnumerable<TestSet> GetTestSets()
+            {
+                yield return Create3InputSet(3, 5, new[] { 2, 1, 5, 1, 2, 2, 2 }, 6);
+            }
+        }
+    }
+}
---
+++ codility/Lessons/Lesson14/NailingPlanks.cs
@@ -0,0 +1,25 @@
+﻿using codility.TestFramework;
+using System;
+using System.Collections.Generic;
+
+namespace codility.Lessons.Lesson14
+{
+    class NailingPlanks : ITestee
+    {
+        int Solve(int[] A, int[] B, int[] C)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Run(params object[] args)
+            => Solve((int[])args[0], (int[])args[1], (int[])args[2]);
+
+        public class Tester : BaseSelfTester<NailingPlanks>
+        {
+            public override IEnumerable<TestSet> GetTestSets()
+            {
+                yield return Create3InputSet(new[] { 1, 4, 5, 8 }, new[] { 4, 5, 9, 10 }, new[] { 4, 6, 7, 10, 2 }, 4);
+            }
+        }
+    }
+}
