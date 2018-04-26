dcc51b53d388d7369aa85a5b4f002bc121e85fae
lincolnyu <g@b1UtLhb9eN>
2018-04-27 00:05:44 +1000

    Add stubs for the rest of Lesson 90.

---
+++ codility/Lessons/Lesson90/FloodDepth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using codility.TestFramework;
+
+namespace codility.Lessons.Lesson90
+{
+    public class FloodDepth : ITestee
+    {
+        public int Solve(int[] A)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object Run(params object[] args)
+            => Solve((int[])args[0]);
+
+        public class Tester : BaseSelfTester<FloodDepth>
+        {
+            public override IEnumerable<TestSet> GetTestSets()
+            {
+                yield return CreateSingleInputSet(new[] { 5, 8 }, 0);
+                yield return CreateSingleInputSet(new[] { 1, 3, 2, 1, 2, 1, 5, 3, 3, 4, 2 }, 2);
+            }
+        }
+    }
+}
---
+++ codility/Lessons/Lesson90/SlalomSkiing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using codility.TestFramework;
+
+namespace codility.Lessons.Lesson90
+{
+    class SlalomSkiing : ITestee
+    {
+        int Solve(int[] A)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object Run(params object[] args)
+            => Solve((int[])args[0]);
+
+        public class Tester : BaseSelfTester<SlalomSkiing>
+        {
+            public override IEnumerable<TestSet> GetTestSets()
+            {
+                yield return CreateSingleInputSet(new[] { 1, 5 }, 2);
+                yield return CreateSingleInputSet(new[] {15,13,5,7,4,10,12,8,2,11,6,9,3 }, 13);
+            }
+        }
+    }
+}
