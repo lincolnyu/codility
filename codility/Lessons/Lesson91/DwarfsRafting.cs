fb4c02be8b9feb452a096ffddc1687c6722e1e9a
Lincoln Yu <quanben@msn.com>
2018-05-20 21:06:51 +1000

    Add stub for lesson 91

---
+++ codility/Lessons/Lesson91/DwarfsRafting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using codility.TestFramework;
+
+namespace codility.Lessons.Lesson91
+{
+    class DwarfsRafting : ITestee
+    {
+        public int solution(int N, string S, string T)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object Run(params object[] args)
+            => solution((int)args[0], (string)args[1], (string)args[2]);
+
+        public class Tester : BaseSelfTester<DwarfsRafting>
+        {
+            public override IEnumerable<TestSet> GetTestSets()
+            {
+                yield return Create3InputSet(4, "1B 1C 4B 1D 2A", "3B 2D", 6);
+            }
+        }
+    }
+}
