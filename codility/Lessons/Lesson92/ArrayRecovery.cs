18d0358ddb5f2f1f96797e76dd8974d191ed0231
lincolnyu <g@b1UtLhb9eN>
2018-07-01 02:26:21 +1000

    Create stub for ArrayRecovery.

---
+++ codility/Lessons/Lesson92/ArrayRecovery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using codility.TestFramework;
+
+namespace codility.Lessons.Lesson92
+{
+    class ArrayRecovery : BaseTestee
+    {
+        public int solution(int[] B, int M)
+        {
+            throw new NotImplementedException();
+        }
+
+        public class Tester : BaseSelfTester<ArrayRecovery>
+        {
+            public override IEnumerable<TestSet> GetTestSets()
+            {
+                yield return CreateInputSet(3, new[] { 0, 2, 2 }, 4);
+                yield return CreateInputSet(4, new[] { 0, 3, 5, 6 }, 10);
+                yield return CreateInputSet(49965, new[] { 0, 0 }, 100000);
+            }
+        }
+    }
+}
